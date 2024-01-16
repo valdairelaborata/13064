cea471306e57d920f0f1d7e12e983025ef203e34
valdair <valdairelaborata@gmail.com>
2024-01-16 20:03:01 -0300

    lab

--- lab.py
+++ lab.py
@@ -4,3 +4,43 @@
 # Além dos métodos mágicos, implemente também os seguintes métodos:
 # depositar(self, valor): Adiciona um valor ao saldo da conta.
 # sacar(self, valor): Retira um valor do saldo da conta, desde que haja saldo suficiente..
+
+
+
+class ContaBancaria:
+    def __init__(self, numero, titular, saldo):
+        self.__numero = numero
+        self.__titular = titular
+        self.__saldo = saldo
+        self.__str__()
+
+    @property
+    def saldo(self):
+       return self.__saldo
+
+    def trocar_titular(self, titular, documento):
+        self.__titular = titular
+        print(f"Trocando o titular {documento}")
+
+    def deposito(self, valor):
+        self.__saldo += valor
+        self.__str__()
+
+    def sacar(self, valor):
+        if self.__saldo < valor:
+            print(f"Saldo insuficiente: {self.__saldo}")
+        else:
+            self.__saldo -= valor
+            self.__str__()
+
+    def __str__(self):
+         print(f"Conta de {self.__titular}: {self.__saldo}")
+
+
+
+conta = ContaBancaria("12345",  "João", 1000)
+# print(conta)
+conta.deposito(500)
+# print(conta)
+conta.sacar(200)
+# print(conta)
