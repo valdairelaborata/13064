4e5ee444d0ab254ccfbc924f0b5b697326d8cd7d
valdair <valdairelaborata@gmail.com>
2024-01-16 20:31:01 -0300

    aula-04

--- main.py
+++ main.py
@@ -1,37 +1,17 @@
 
-class Veiculo(object):
-    def __init__(self, modelo, placa, valor) :
-        self.modelo = modelo
-        self.placa = placa
-        self.valor = valor
+def aplicar(funcao, valor):
+    return funcao(valor)
 
-    # def __del__(self):
-    #     print(f"Objeto {self.modelo} sendo destruído")
-   
-    # def __str__(self):
-    #     return f"Veículo {self.modelo} - placa {self.placa}"
+def dobrar(numero):
+    return numero * 2
 
-    # def __repr__(self) :
-    #     return f"Veículo (modelo='{self.modelo}', placa='{self.placa}')"
+def triplicar(numero):
+    return numero * 3
 
-    def __add__(self, outro):
-        return self.valor + outro.valor
-    
-    def __eq__(self, outro) :
-        return self.modelo == outro.modelo
-    
-    def __ne__(self, outro):
-        return not self.__eq__(outro)
-    
-    
-   
-veiculo1 = Veiculo("Celta", "AKH 0725", 12000)
-veiculo2 = Veiculo("Fusca", "AKH 0725", 12000)
 
-# print(veiculo1 + veiculo2)
-
-# print(veiculo1 == veiculo2)
-
-print(veiculo1 != veiculo2)
+resultado1 = aplicar(dobrar, 5)
+print(resultado1)
+resultado2 = aplicar(triplicar, 5)
+print(resultado2)
 
 print("Fim")
