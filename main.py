8918959e5d86a6fb73d8a6dff221ce0a4cb3b9b6
valdair <valdairelaborata@gmail.com>
2024-02-02 20:44:10 -0300

    lab

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 
-from fastapi import FastAPI
+from datetime import datetime, timedelta
+import jwt
+from fastapi.security import OAuth2PasswordBearer
+
+from fastapi import FastAPI, Depends, HTTPException
 from pydantic import BaseModel
 
 from services.produto import criar as criarProduto
@@ -10,10 +14,41 @@ from services.produto import excluir as excluirProduto
 
 app = FastAPI()
 
+SECRET_KEY = "mysecretkey"
+ALGORITHM = "HS256" 
+ACCESS_TOKEN_EXPIRE_MINUTES = 10
+
 
 class ProdutoView(BaseModel):
-    codigo: str
+    codigo: str 
     descricao: str
+
+
+def gerarToken(data: dict)  -> str:
+    to_encode = data.copy()
+    expires = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    to_encode.update({"exp": expires})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
+
+
+def validaToken(jwt: str):
+    try:
+        payload = jwt.decode(jwt, SECRET_KEY, algorithm=ALGORITHM)   
+        return payload     
+    except Exception:
+        raise  HTTPException(status_code=401, detail="Acesso negado!")
+
+
+
+@app.post("/token")
+async def obterToken(usuario, senha):
+
+    token_data = {"sub": usuario, "scopes": ["me"]}
+    token = gerarToken(token_data)
+
+    return {"access_token": token}
+
 
 
 #CRUD
@@ -27,7 +62,7 @@ def criar(produtoView: ProdutoView):
 
 #R - Ler
 @app.get("/produtos/{id}")
-def buscar(id: int):
+def buscar(id: int, usuario: dict = Depends(validaToken)):
     return buscarProduto(id)
 
 
@@ -43,16 +78,3 @@ def alterar(id:int, produtoView: ProdutoView):
 def delete(id: int):
     excluirProduto(id)
     return {"mensagem": f"Produto excluído. {id}"}
-
-
-
-# from services.cliente import alterar, listar_contas
-# from services.contabancaria import adicionar
-
-# # adicionar("Claúdia", "claudia@gmail.com")
-
-# alterar(3, "Ana Cláudia")
-
-# # listar_contas(3)
-
-# # adicionar(987, 8000, 3)
